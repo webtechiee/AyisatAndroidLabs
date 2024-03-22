5151483347d4c78ac18c5f2a61acbf84831deeac
webtechiee <minaj1159@gmail.com>
2024-03-21 20:22:56 -0400

    Finished test cases and Javadocs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,9 @@ android {
         viewBinding=true
     }
 
-        defaultConfig {
+
+
+    defaultConfig {
         applicationId = "algonquin.cst2335.ayisat"
         minSdk = 22
         targetSdk = 34
@@ -37,6 +39,8 @@ android {
     }
 }
 
+
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,8 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
 
 
 }
