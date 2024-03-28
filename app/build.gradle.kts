55b9d50ed46a923b320f5627684d5a0ac81eb842
webtechiee <minaj1159@gmail.com>
2024-03-28 19:48:04 -0400

    Week 7 completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,10 @@ android {
                 "proguard-rules.pro"
             )
         }
+        buildFeatures {
+            viewBinding=true;
+        }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -32,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.android.volley:volley:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/algonquin/cst2335/ayisat/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ayisat/MainActivity.java
@@ -1,79 +1,138 @@
 package algonquin.cst2335.ayisat;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.net.URLEncoder.encode;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import java.text.BreakIterator;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import algonquin.cst2335.ayisat.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
-    private static final String TAG = "MainActivity";
-    private SharedPreferences prefs;
+    protected RequestQueue queue = null;
+    protected String cityName;
+    final protected  String API_KEY="31e20d886922cc1856308ed89aae685d";
+    private ImageLoader imageLoader;
+    private JSONObject response;
 
+    @SuppressLint("SuspiciousIndentation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.w("MainActivity", "In onCreate() - Loading Widgets");
-        Log.w(TAG, "onCreate: Activity is being created");
 
-        // Initialize SharedPreferences
-        prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
-
-        EditText emailEditText = findViewById(R.id.emailEditText);
-        String emailAddress = prefs.getString("LoginName", "");
-        emailEditText.setText(emailAddress);
-
-        Button loginButton = findViewById(R.id.button);
-        loginButton.setOnClickListener(v -> {
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString("LoginName", emailEditText.getText().toString());
-            editor.apply();
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
-            // Start SecondActivity
-            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-            nextPage.putExtra("EmailAddress", emailEditText.getText().toString());
-            startActivity(nextPage);
+
+        binding.btnGet.setOnClickListener(e->{
+
+
+            cityName = binding.edtCity.getText().toString();
+
+            queue = Volley.newRequestQueue(this);
+
+            imageLoader = new ImageLoader(queue, new ImageLoader.ImageCache() {
+                @Override
+                public Bitmap getBitmap(String url) {
+                    return null;
+                }
+
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+                    // No need to implement in this example
+                }
+            });
+
+            String stringURL = null;
+            try {
+                stringURL = "https://api.openweathermap.org/data/2.5/weather?q=" + encode(cityName,  "UTF-8") + "&appid="+API_KEY+ "&units=metric";
+            } catch (UnsupportedEncodingException ex) {
+                throw new RuntimeException(ex);
+            }
+
+
+            Log.w("mervat1", stringURL);
+
+
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+                    (response) -> {
+
+                        JSONObject mainObject = null;
+
+                        JSONObject weatherObject = null;
+
+                        double current ;
+                        double min ;
+                        double max ;
+                        int humidity ;
+
+                        String iconCode;
+
+                        try {
+
+                            mainObject = response.getJSONObject("main");
+                            current = mainObject.getDouble("temp");
+                            min = mainObject.getDouble("temp_min");
+                            max = mainObject.getDouble("temp_max");
+                            humidity = mainObject.getInt("humidity");
+
+                            JSONArray weatherArray = response.getJSONArray("weather");
+                            if (weatherArray.length() > 0) {
+                                weatherObject = weatherArray.getJSONObject(0);
+                                iconCode = weatherObject.getString("icon");
+                                String iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+                                ImageLoader.ImageListener listener = ImageLoader.getImageListener(binding.img, 0, 0);
+                                imageLoader.get(iconUrl, listener);
+
+                            }
+
+
+                        } catch (JSONException ex) {
+                            Log.w("mervat","4");
+                            throw new RuntimeException(ex);
+                        }
+
+
+                        runOnUiThread( ( ) -> {
+                            binding.temp.setText("The current temperature is:  " + current);
+                            binding.temp.setVisibility(View.VISIBLE);
+                            binding.minTemp.setText("The min temperature is:  " + min);
+                            binding.minTemp.setVisibility(View.VISIBLE);
+                            binding.maxTemp.setText("The max temperature is:  " + max);
+                            binding.maxTemp.setVisibility(View.VISIBLE);
+                            binding.humidityTemp.setText("The humidity temperature is:  " + humidity);
+                            binding.humidityTemp.setVisibility(View.VISIBLE);
+                            // do this for all the textViews...
+                        });
+
+                    }, (error) -> { });
+            queue.add (request);
+
+
+
 
         });
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w(TAG, "onStart: The application is now visible on screen");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w(TAG, "onResume: The application is now responding to user input");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w(TAG, "onPause: Activity is partially visible to the user");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w(TAG, "onStop: Activity is no longer visible to the user");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w(TAG, "onDestroy: Activity is being destroyed");
     }
 }
